92232a7c80615a15a66777fc00c89f85c178dee4
Longchuan Niu <linux88us@gmail.com>
2020-12-14 02:31:04 +0200

    video.py completed

--- process.py
+++
@@ -1,8 +0,0 @@
-      cv2_im = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-      pil_im = Image.fromarray(cv2_im)
-      
-      # DeepLAb model output
-      resized_im, seg_map = MODEL.run(pil_im)
-      arr = np.asarray(seg_map)
-
-      seg_image = label_to_color_image(seg_map).astype(np.uint8)
--- seg_prossesing.py
+++
@@ -1,4 +0,0 @@
-      seg_image = seg_image[:,:,0] > 100 
-      seg_image = np.uint8(seg_image)*255
-      seg_image = cv2.cvtColor(seg_image, cv2.COLOR_GRAY2BGR)
-      seg_image = 255 - seg_image
